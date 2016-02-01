f38abffe0810dc9bbc7dc74c51585058788e29e3
Maxim Ryndin <masim_r@mail.ru>
2016-02-01 13:27:24 +0300

    Add graph class stub for now.

---
+++ src/SPP/Graph.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SPP;
+
+class Graph implements GraphInterface
+{
+
+    protected $vertices = array();
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addVertex($vertex)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addEdge($from, $to, $cost)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addEdgeForce($from, $to, $cost)
+    {
+
+    }
+}
+
+?>
