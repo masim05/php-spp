87dcec438b8a1156dfa68d91e64cfcd93e291d7c
Maxim Ryndin <masim_r@mail.ru>
2016-02-01 13:23:36 +0300

    Add dijkstra test, broken yet.

---
+++ test/SPP/DijkstraTest.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use SPP\Graph;
+use SPP\DijkstraSPPSolver;
+
+class DijkstraTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testSolutionInConnectedGraph()
+    {
+        $graph = new Graph();
+
+        $graph->addEdgeForce('Moscow', 'Kaluga', 10);
+        $graph->addEdgeForce('Moscow', 'Tula', 12);
+        $graph->addEdgeForce('Moscow', 'Ryazan', 14);
+        $graph->addEdgeForce('Moscow', 'Vladimir', 16);
+        $graph->addEdgeForce('Kaluga', 'Tula', 4);
+        $graph->addEdgeForce('Ryazan', 'Tula', 10);
+        $graph->addEdgeForce('Ryazan', 'Vladimir', 16);
+
+        $solver = new DijkstraSPPSolver($graph);
+
+        $solution = $solver->solve('Kaluga', 'Vladimir');
+
+        //print_r($solution);
+
+        $this->assertEquals(true, $solution["success"]);
+        $this->assertEquals(26, $solution["cost"]);
+
+    }
+}
+
+?>
